64f41a82b98b81741761440ee67b0fd49ba07421
Dórian Langbeck <langbeck@hpe.com>
2016-11-10 11:01:57 -0200

    Fix: "git apply" instead of "git am" (again)

--- sfc-demo/setup.sh
+++ sfc-demo/setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 if [ -z "${http_proxy}" ]; then
     echo "http_proxy environment not defined"
@@ -31,9 +32,7 @@ git reset --hard f65065b19516a750b73262c63ba269fca2365e23
 
 # Apply patches required to add proxy support
 cp $PATCH_DIR/*.patch ./
-git config user.email odl@opendaylight.org
-git config user.name odl
-git am *.patch
+git apply *.patch
 
 # Configure proxy environment files (docker, apt, profile, sudoers, etc)
 cd sfc-demo/sfc103
